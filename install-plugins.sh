3d83635ffd596bc258651a15e6a80e3ee6eddc9b
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2022-11-16 23:09:14 -0800

    add custom normalise plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -4,3 +4,4 @@ while read plugin version; do
   echo "Installing '${plugin}' (${version})"
   pip install git+https://github.com/alerta/alerta-contrib.git@${version}#subdirectory=${plugin}
 done </app/plugins.txt
+pip install git+https://github.com/michael-chambers/alerta-contrib.git@normalise_plugin#subdirectory=plugins/normalise
