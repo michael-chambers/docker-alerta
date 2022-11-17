b03fc43dc60fa4717a8b5de586cbdb10b0f653f1
michael-chambers <56092496+michael-chambers@users.noreply.github.com>
2022-11-17 12:57:37 -0800

    remove mirantis-normalise plugin install

--- install-plugins.sh
+++ install-plugins.sh
@@ -4,4 +4,3 @@ while read plugin version; do
   echo "Installing '${plugin}' (${version})"
   pip install git+https://github.com/alerta/alerta-contrib.git@${version}#subdirectory=${plugin}
 done </app/plugins.txt
-pip install git+https://github.com/michael-chambers/alerta-contrib.git@normalise_plugin#subdirectory=plugins/normalise
